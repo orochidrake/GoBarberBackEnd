bfe42f8f830b9a80e3bbaace47f4e736afc5f177
andre <andre@ulend.com.br>
2020-07-08 19:26:37 -0300

    refactor-structor-to-ddd

---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
+import UsersControllers from '../controllers/UsersControllers';
+import UserAvatarController from '../controllers/UserAvatarController';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+const usersControllers = new UsersControllers();
+const userAvatarController = new UserAvatarController();
+
+usersRouter.post('/', usersControllers.create);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
+
+export default usersRouter;
